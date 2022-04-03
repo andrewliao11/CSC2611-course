a416a1fcd06d60fb7c96ad2c649bff88eae3f3c8
Andrew <andrewliao11@gmail.com>
2022-04-03 00:52:33 -0400

    add

--- project/datasets/semeval2020_ulscd_eng/full_corpus/split_train_val.py
+++ project/datasets/semeval2020_ulscd_eng/full_corpus/split_train_val.py
@@ -9,8 +9,9 @@ np.random.seed(seed)
 
 
 def split_train_val(train_ratio=0.8):
-    dataset_dir = Path('/home/andliao/workspace/course/csc2611-course-semantic-change/project/datasets/semeval2020_ulscd_eng/full_corpus')
-    
+    #dataset_dir = Path('/home/andliao/workspace/course/csc2611-course-semantic-change/project/datasets/semeval2020_ulscd_eng/full_corpus')
+    dataset_dir = Path('')
+
     corpus = []
     source = []
     for p in dataset_dir.glob('*.txt'):
@@ -19,33 +20,33 @@ def split_train_val(train_ratio=0.8):
         corpus.extend(lowercase_lines)
         source.extend(len(lines) * [p.name])
 
-        
+
     train_mask = np.zeros(len(corpus), dtype=bool)
     train_idx = np.random.choice(len(corpus), int(train_ratio * len(corpus)), replace=False)
     train_mask[train_idx] = True
-    
+
     train_corpus = [l for m, l, s in zip(train_mask, corpus, source) if m and len(l) > 0]
     with (dataset_dir / 'train' / 'train.txt').open('w') as f:
         for l in train_corpus:
             f.writelines(l + '\n')
-        
+
     train_source = [s for m, l, s in zip(train_mask, corpus, source) if m and len(l) > 0]
     with (dataset_dir / 'train' / 'train_source.txt').open('w') as f:
         for l in train_source:
             f.writelines(l + '\n')
-    
-    
+
+
     val_corpus = [l for m, l, s in zip(train_mask, corpus, source) if not m and len(l) > 0]
     with (dataset_dir / 'val' / 'val.txt').open('w') as f:
         for l in val_corpus:
             f.writelines(l + '\n')
-    
+
     val_source = [s for m, l, s in zip(train_mask, corpus, source) if not m and len(l) > 0]
     with (dataset_dir / 'val' / 'val_source.txt').open('w') as f:
         for l in val_source:
             f.writelines(l + '\n')
-    
-    
+
+
 if __name__ == '__main__':
     split_train_val()
-    
+
